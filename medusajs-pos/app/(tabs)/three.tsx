2aca75c9973e3264ce196559dbb0c07ef1bb2f81
Scott William Jones <info@netgurusolutions.co.uk>
2025-01-28 10:49:03 +0000

    Clean Template for POS

---
+++ medusajs-pos/app/(tabs)/three.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+import { View } from '@/components/Themed';
+
+export default function TabThreeScreen() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
