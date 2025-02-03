96749f680d0f95e8e18b77e00c07abac5bb041cb
Scott William Jones <info@netgurusolutions.co.uk>
2025-02-03 14:12:33 +0000

    removal of mdeusaj-pos

--- medusajspos/lib/config.ts
+++
@@ -1,9 +0,0 @@
-import Medusa from "@medusajs/js-sdk"
-
-export const sdk = new Medusa({
-    baseUrl: "http://192.168.68.129:9000",
-    debug: process.env.NODE_ENV === "development",
-    auth: {
-        type: "session",
-    },
-})
